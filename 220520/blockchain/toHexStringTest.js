236b3b17e71db53a292a9dfb573fd5bcdfceedf3
parkisak1029 <isaaac1029@gmail.com>
2022-05-20 13:55:29 +0900

    220520

---
+++ 220520/blockchain/toHexStringTest.js
@@ -0,0 +1,8 @@
+const toHexString = (byteArray) => {
+
+    return Array.from(byteArray, (byte) => {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2)
+    }).join('')
+}
+
+console.log(toHexString([1,79]))
