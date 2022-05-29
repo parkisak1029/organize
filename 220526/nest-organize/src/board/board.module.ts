be18b3f5adbe88a9fb97e4bb47517193f4e45f6b
parkisak1029 <isaaac1029@gmail.com>
2022-05-29 23:27:08 +0900

    220529

--- 220526/nest-organize/src/board/board.module.ts
+++ 220526/nest-organize/src/board/board.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardController } from './board.controller';
+import { BoardRepository } from './board.repository';
 import { BoardService } from './board.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardController],
   providers: [BoardService],
 })
-export class BoardModule {}
+export class BoardModule { }
