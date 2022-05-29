b037d73a27c9c1d183a8dc489bde9dcf7bcc053c
parkisak1029 <isaaac1029@gmail.com>
2022-05-29 23:30:07 +0900

    220529

--- 220526/nest-organize/src/board/board.controller.ts
+++ 220526/nest-organize/src/board/board.controller.ts
@@ -1,43 +1,43 @@
 import { Controller, Get, Post, Body, Param, Delete, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board.status.enum';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create.board.dto';
 import { BoardStatusValidationPipe } from './pipe/board-status-validation.pipe.dot';
 
 @Controller('board')
 export class BoardController {
-  constructor(private boardService: BoardService) { }
-  //private 선언시 암묵적으로 프로퍼티로 선언된다0.
+  // constructor(private boardService: BoardService) { }
+  // //private 선언시 암묵적으로 프로퍼티로 선언된다0.
 
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardService.getAllBoards();
-  }
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardService.getAllBoards();
+  // }
 
-  @Post()
-  @UsePipes(ValidationPipe)
-  //ValidationPipe 유효성 검사해주는 파이프
-  createBoard(
-    @Body() createBoardDto: CreateBoardDto
-  ): Board {
-    return this.boardService.createBoard(createBoardDto)
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // //ValidationPipe 유효성 검사해주는 파이프
+  // createBoard(
+  //   @Body() createBoardDto: CreateBoardDto
+  // ): Board {
+  //   return this.boardService.createBoard(createBoardDto)
+  // }
 
-  @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
-    return this.boardService.getBoardById(id)
-  }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardService.getBoardById(id)
+  // }
 
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardService.deleteBoard(id);
-  }
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id: string): void {
+  //   this.boardService.deleteBoard(id);
+  // }
 
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus
-  ) {
-    return this.boardService.updateBoardStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus
+  // ) {
+  //   return this.boardService.updateBoardStatus(id, status);
+  // }
 }
--- 220526/nest-organize/src/board/board.service.ts
+++ 220526/nest-organize/src/board/board.service.ts
@@ -1,45 +1,44 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board.status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create.board.dto';
 
 @Injectable()
 export class BoardService {
-  private board: Board[] = [];
 
-  getAllBoards(): Board[] {
-    return this.board;
-  }
+  // getAllBoards(): Board[] {
+  //   return this.board;
+  // }
 
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto
-    const board: Board = {
-      id: uuid(), // uuid module은 unique한 값을 넣어줌
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   const { title, description } = createBoardDto
+  //   const board: Board = {
+  //     id: uuid(), // uuid module은 unique한 값을 넣어줌
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
 
-    this.board.push(board);
-    return board;
-  }
+  //   this.board.push(board);
+  //   return board;
+  // }
 
-  getBoardById(id: string): Board {
-    const found = this.board.find((board) => board.id === id);
-    if (!found) {
-      throw new NotFoundException('id값이 틀림');
-    }
-    return found;
-  }
+  // getBoardById(id: string): Board {
+  //   const found = this.board.find((board) => board.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException('id값이 틀림');
+  //   }
+  //   return found;
+  // }
 
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.board = this.board.filter((board) => board.id !== found.id);
-  }
+  // deleteBoard(id: string): void {
+  //   const found = this.getBoardById(id);
+  //   this.board = this.board.filter((board) => board.id !== found.id);
+  // }
 
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board = this.getBoardById(id);
+  //   board.status = status;
+  //   return board;
+  // }
 }
