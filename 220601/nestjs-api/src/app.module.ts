0ec56028afaa3817696a3298140d35ed44ef4288
parkisak1029 <isaaac1029@gmail.com>
2022-06-01 22:59:20 +0900

    220601

--- 220601/nestjs-api/src/app.module.ts
+++ 220601/nestjs-api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Authmodule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [Authmodule, UserModule, BookmarkModule],
 })
-export class AppModule {}
+export class AppModule { }
