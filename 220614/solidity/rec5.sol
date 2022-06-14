715541d8c0cd68c10fb48a188a8dae91000e7598
parkisak1029 <isaaac1029@gmail.com>
2022-06-14 14:12:37 +0900

    220614

--- 220614/solidity/rec5.sol
+++ 220614/solidity/rec5.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract A {
-    constructor(string memory _givenName) public {
-        givenName = _givenName;
-    }
-
     string public familyName = "Kim";
     string public givenName = "Suck";
     uint256 public money = 100;
+
+    constructor(string memory _givenName) public {
+        givenName = _givenName;
+    }
 
     function getFamilyName() public view returns (string memory) {
         return familyName;
@@ -18,12 +18,20 @@ contract A {
         return givenName;
     }
 
-    function getMoney() public view returns (uint256) {
+    function getMoney() public view virtual returns (uint256) {
         return money;
     }
 }
 
 // 앞에 오는 B가 상속자
 contract B is A("James") {
+    uint256 public earning = 0;
 
+    function work() public {
+        earning += 100;
+    }
+
+    function getMoney() public view override returns (uint256) {
+        return money + earning;
+    }
 }
