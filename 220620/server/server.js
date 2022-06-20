367174cf86e48bee8185c4f3672e21336924c772
parkisak1029 <isaaac1029@gmail.com>
2022-06-20 12:16:08 +0900

    220620

--- 220620/server/server.js
+++ 220620/server/server.js
@@ -3,9 +3,16 @@ const express = require('express')
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
+const { uploadFile } = require('./s3')
+
 const app = express()
 
-app.post('/images', upload.single('image'), (req, res) => {
+app.post('/images', upload.single('image'), async (req, res) => {
+    const file = req.file
+    console.log(file)
+    const result = await uploadFile(file)
+    console.log(result)
+    const description = req.body.description
     res.send('okay')
 })
 
