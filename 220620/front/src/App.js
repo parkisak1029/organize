40f7a5df78a5fb199c76766d5ba3e33662f7b3cb
parkisak1029 <isaaac1029@gmail.com>
2022-06-20 11:08:40 +0900

    220620

--- 220620/front/src/App.js
+++ 220620/front/src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+async function postImage({ image, description }) {
+  const formData = new FormData();
+  formData.append("image", image)
+  formData.append("description", description)
+
+  const result = await axios.post('/images', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+  return result.data
+}
+
 
 function App() {
+
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  const submit = async event => {
+    event.preventDefault()
+    const result = await postImage({ image: file, description })
+    setImages([result.image, ...images])
+  }
 
   const fileSelected = event => {
     const file = event.target.files[0]
     setFile(file)
   }
+
   return (
     <div className="App">
       <form onSubmit={submit}>
@@ -20,6 +43,7 @@ function App() {
           <img src={image}></img>
         </div>
       ))}
+
     </div>
   );
 }
