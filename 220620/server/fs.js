c1e418ec83de12a579b5cd03300989dd718e5cf1
parkisak1029 <isaaac1029@gmail.com>
2022-06-21 15:33:19 +0900

    220621

--- 220620/server/fs.js
+++ 220620/server/fs.js
@@ -1,35 +1,45 @@
 var fs = require("fs");
-const { uploadFile, getFileStream, deleteFile } = require('./s3')
+const { uploadFile, getFileStream } = require('./s3')
 const S3 = require('aws-sdk/clients/s3')
 require('dotenv').config()
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
 
 const baseURI = 'buf\\';
 
-// for (let i = 1; i < 6; i++) {
-fs.readFile(`./image/1.png`, async function (err, data) {
-    if (err) throw err;
-    // Encode to base64
-    var encodedImage = new Buffer(data, 'binary');
+let num1 = [0, 0, 33, 66, 99, 122]
+let num2 = [0, 32, 65, 98, 121, 154]
 
-    await fs.writeFileSync(`./buf/1`, encodedImage);
-    let file = {
-        path: `${baseURI}1`,
-        filename: `1`
-    }
-    // console.log('file : ', file)
-    let tdata = await uploadFile(file);
-    s3.deleteObject({
-        Bucket: 'mybucket', // 사용자 버켓 이름
-        Key: 'image/helloworld.jpeg' // 버켓 내 경로
-    }, (err, data) => {
-        if (err) { throw err; }
-        console.log('s3 deleteObject ', data)
-    })
-    console.log('tdata : ', tdata);
-    // console.log(encodedImage);
 
-    // Decode from base64
-    var decodedImage = new Buffer(encodedImage, 'base64').toString('binary');
-});
-// }
+for (let i = 1; i < 6; i++) {
+    fs.readFile(`./image/${i}.png`, async function (err, data) {
+        if (err) throw err;
+        var encodedImage = new Buffer(data, 'binary');
+        let hash = encodedImage.toString('base64')
+        let rep = hash.replaceAll('/', '').replaceAll('+', '').replaceAll('=', '')
+        let len = rep.length
+
+        let hashCode = await rep.slice(len - 32)
+        fs.writeFileSync(`./buf/${hashCode}`, encodedImage);
+        // Encode to base64
+        let file = {
+            path: `${baseURI}${hashCode}`,
+            filename: `${hashCode}`
+        }
+        console.log('file : ', file)
+        let tdata = await uploadFile(file);
+        console.log('tdata : ', tdata);
+
+        // Decode from base64
+        var decodedImage = new Buffer(encodedImage, 'base64').toString('binary');
+    });
+}
 
