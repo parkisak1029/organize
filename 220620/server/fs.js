c57f3466ee0dfd29ded0d1119709dcd3248a320e
parkisak1029 <isaaac1029@gmail.com>
2022-06-21 11:11:56 +0900

    220621

--- 220620/server/fs.js
+++ 220620/server/fs.js
@@ -3,22 +3,28 @@ const { uploadFile, getFileStream } = require('./s3')
 
 const baseURI = 'buf\\';
 
-fs.readFile('./img/images/10.png', async function (err, data) {
+// for (let i = 1; i < 6; i++) {
+fs.readFile(`./image/1.png`, async function (err, data) {
     if (err) throw err;
-
     // Encode to base64
     var encodedImage = new Buffer(data, 'binary');
 
-    fs.writeFileSync('./buf/10', encodedImage);
-    let file = {
-        path: `${baseURI}10`,
-        filename: '10'
+    await fs.writeFileSync(`./buf/1`, encodedImage);
+    let files = {
+        path: `${baseURI}1`,
+        filename: `1`
     }
-    console.log(file)
+    let file = {
+        path: `${baseURI}${files.ETag}`,
+        filename: `${files.ETag}`
+    }
+    // console.log('file : ', file)
     let tdata = await uploadFile(file);
-    console.log(tdata);
+    console.log('tdata : ', tdata);
     // console.log(encodedImage);
 
     // Decode from base64
-    //   var decodedImage = new Buffer(encodedImage, 'base64').toString('binary');
+    var decodedImage = new Buffer(encodedImage, 'base64').toString('binary');
 });
+// }
+
